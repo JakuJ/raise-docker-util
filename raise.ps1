6954f3e2cce323fdc907a5f5cc79a217495b73d1
stinem1 <54391072+stinem1@users.noreply.github.com>
2024-08-29 13:38:57 +0200

    ps1: Find the full path name

--- raise.ps1
+++ raise.ps1
@@ -1,11 +1,11 @@
 $CMDS = $args[0..($args.Count - 2)]
 $ARG = $args[-1]
-$DIR = Split-Path -Parent $ARG
-$REALDIR = (Get-Item -LiteralPath $DIR).FullName -replace '\\', '/'
-$NAME = Split-Path -Leaf $ARG
+$REALDIR = (Get-Item -LiteralPath $ARG).FullName -replace '\\', '/'
+$DIR = Split-Path -Parent $REALDIR
+$NAME = Split-Path -Leaf $REALDIR
 
 # Convert the file to LF line endings
-(Get-Content -Raw -Path $ARG) -replace "`r`n", "`n" | Set-Content -NoNewline -Force -Path $ARG
+(Get-Content -Raw -Path $REALDIR) -replace "`r`n", "`n" | Set-Content -NoNewline -Force -Path $REALDIR
 
 # Run the Docker command with the correctly formatted path
-docker run --rm -v "${REALDIR}:/usr/src" ghcr.io/jakuj/raise-tools:main $CMDS "./$NAME"
+docker run --rm -v "${DIR}:/usr/src" ghcr.io/jakuj/raise-tools:main $CMDS "./$NAME"
